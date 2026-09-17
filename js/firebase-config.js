5f0f960cf6c06fb1ea4800f23a95cdabdbb3856f
segundogarden <segundogarden@gmail.com>
2026-09-17 09:21:11 -0300

    versao 1

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD1tRtZYYnleMWUXkJF9n7E1FIcAt5Fgzc",
-    authDomain: "professorjackson-f41a2.firebaseapp.com",
-    projectId: "professorjackson-f41a2",
+  apiKey: "AIzaSyBUFhX9SXAG7fut2V3zQuipSJvjh_nOgWI",
+  authDomain: "cosmicgarou-402a0.firebaseapp.com",
+  projectId: "cosmicgarou-402a0",
+  storageBucket: "cosmicgarou-402a0.firebasestorage.app",
+  messagingSenderId: "149282337221",
+  appId: "1:149282337221:web:0ebf2b341ee515d6c29bac"
 };
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
 
-const storage = firebase.storage();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
